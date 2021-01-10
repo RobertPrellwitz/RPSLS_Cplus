38b797854774b3948650fde283d0efe18db97d31
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-10 13:43:16 -0600

    Player class added

---
+++ Player.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Robert Prellwitz on 1/10/21.
+//
+
+#include "Player.h"
---
+++ Player.h
@@ -0,0 +1,14 @@
+//
+// Created by Robert Prellwitz on 1/10/21.
+//
+
+#ifndef RPSLS_CPLUS_PLAYER_H
+#define RPSLS_CPLUS_PLAYER_H
+
+
+class Player {
+
+};
+
+
+#endif //RPSLS_CPLUS_PLAYER_H

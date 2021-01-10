1bb948e04d5495c18d9649352a8f93e4321835ab
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-10 13:35:09 -0600

    Intro added to Main class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Welcome to the classic variation of Rshambo as popularized by Sheldon of the Big Bang Theory!"
+            "\nRock, Paper, Scissors, Lizard, Spock!" << endl ;
+    cout << "Game plays is as follows: " << endl << "\n Scissors Cuts Paper \nPaper Cover Rock \nRock crushes Lizard"
+                                                    "\nLizard Poisons Spock \nSpock smashes Scissors \n"
+                                                    "Scissors decapitates Lizard \nLizard eats Paper \nPaper disproves Spock"
+                                                    "\nSpock vaporizes Rock\n(and as it always has!!...)\nRock crushes Scissors";
+
     return 0;
 }

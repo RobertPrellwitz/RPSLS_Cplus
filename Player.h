8925667057d82a6fdfe3c8756ddb42671c49fa9f
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-10 13:59:43 -0600

    added subclasses and base construction

--- Player.h
+++ Player.h
@@ -1,12 +1,24 @@
 //
 // Created by Robert Prellwitz on 1/10/21.
 //
-
+#include <string>
 #ifndef RPSLS_CPLUS_PLAYER_H
 #define RPSLS_CPLUS_PLAYER_H
-
+using namespace std;
 
 class Player {
+public:
+    string name;
+    string choices[5] = {"Rock", "Paper", "Scissors", "Lizard", "Spock"};
+    string gesture;
+    int score = 0;
+};
+
+class Human: public Player{
+
+};
+
+class Computer: public Player{
 
 };
 

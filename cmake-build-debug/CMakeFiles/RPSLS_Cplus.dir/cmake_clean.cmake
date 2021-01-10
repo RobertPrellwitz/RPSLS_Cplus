df3ff71385a61a8a1043587365dca27c437c464a
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-10 13:32:54 -0600

    initial Commit

---
+++ cmake-build-debug/CMakeFiles/RPSLS_Cplus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPSLS_Cplus.dir/main.cpp.o"
+  "RPSLS_Cplus"
+  "RPSLS_Cplus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RPSLS_Cplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

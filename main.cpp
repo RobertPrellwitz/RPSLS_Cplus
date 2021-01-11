6cc6ec62f8337e7ed355e5928393f39e2113f51f
Robert Prellwitz <robprellwitz@uwalumni.com>
2021-01-11 05:42:16 -0600

    added rungame method to main class

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "Game.h"
+#include "Player.h"
 using namespace std;
 int main() {
     cout << "Welcome to the classic variation of Rshambo as popularized by Sheldon of the Big Bang Theory!"
@@ -6,7 +8,9 @@ int main() {
     cout << "Game plays is as follows: " << endl << "\n Scissors Cuts Paper \nPaper Cover Rock \nRock crushes Lizard"
                                                     "\nLizard Poisons Spock \nSpock smashes Scissors \n"
                                                     "Scissors decapitates Lizard \nLizard eats Paper \nPaper disproves Spock"
-                                                    "\nSpock vaporizes Rock\n(and as it always has!!...)\nRock crushes Scissors";
+                                                    "\nSpock vaporizes Rock\n(and as it always has!!...)\nRock crushes Scissors" << endl;
+    Game newGame;
+    newGame.runGame();
 
     return 0;
 }
